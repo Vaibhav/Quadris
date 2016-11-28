14e03308f05c7c3e55b4467be569b6c98ee565cb
Harman Singh <harman.j.singh@hotmail.com>
2016-11-27 23:20:50 -0600

    Fully Implemented CommandInterpreter that works

--- Quadris/Quadris/Game.cc
+++ Quadris/Quadris/Game.cc
@@ -35,21 +35,40 @@ void Game::play() {
 		for (int i = 0; i != numOfCommands; ++i) {
 			if (commands[i].first == "L") {
 				//First argument is multiplier, which is by default 1
+				cerr << "L" << stoi(commands[i].second[0]);
 				b.currentBlockLeft(stoi(commands[i].second[0])); 
 			} else if (commands[i].first == "R") {
+				cerr << "R" << stoi(commands[i].second[0]);
 				b.currentBlockRight(stoi(commands[i].second[0]));
-			} else if (commands[i].first == "L") {
-				b.currentBlockLeft(stoi(commands[i].second[0]));
 			} else if (commands[i].first == "D") {
+				cerr << "D" <<  stoi(commands[i].second[0]);
 				b.currentBlockDown(stoi(commands[i].second[0]));
 			} else if (commands[i].first == "CW") {
+				cerr << "CW" << stoi(commands[i].second[0]);
 				b.currentBlockRotateClockwise(stoi(commands[i].second[0]));
 			} else if (commands[i].first == "CCW") {
+				cerr << "CCW" << stoi(commands[i].second[0]);
 				b.currentBlockRotateCounterClockwise(stoi(commands[i].second[0]));
 			} else if (commands[i].first == "LU") {
+				cerr << "LU" << stoi(commands[i].second[0]);
 				increaseLevel(stoi(commands[i].second[0]));
 			} else if (commands[i].first == "LD") {
+				cerr << "LD" << stoi(commands[i].second[0]);
 				decreaseLevel(stoi(commands[i].second[0]));
+			} else if(commands[i].first == "BLOCK-I"){
+				
+			} else if(commands[i].first == "BLOCK-J"){
+				
+			} else if(commands[i].first == "BLOCK-L"){
+				
+			} else if(commands[i].first == "BLOCK-O"){
+				
+			} else if(commands[i].first == "BLOCK-S"){
+				
+			} else if(commands[i].first == "BLOCK-Z"){
+				
+			} else if(commands[i].first == "BLOCK-T"){
+				
 			}
 		}
 		cout << display;
